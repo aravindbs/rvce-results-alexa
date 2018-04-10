8c0f448e1babd9e2ee8d7ef91ad124ee973914f5
Aravind B.S <bs.aravind.97@gmail.com>
2018-04-11 01:06:59 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import logging, yaml, json
+from flask import Flask, render_template, request, session as flaskSession
+from flask_ask import Ask, statement, question, session
+from scraper import get_result
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+@ask.launch
+def index() :
+    welcome_msg = render_template('welcome')
+    return question(welcome_msg)
+
+@ask.intent('usn', mapping = { 'year' : 'year',  'department' : 'department', 'number' : 'number'}) 
+def getRes(year, department, number):
+
+    YEAR = "%02d"%int(year)
+    DEPT = department
+    NUM = "%03d"%int(number)
+    usn = "1RV{}{}{}".format(YEAR, DEPT, NUM)
+
+    result = get_result (usn)
+
+    if (result == "400") :
+        notFound = render_template("notFound", usn = usn)
+        return statement (notFound).simple_card(title='Not Found', content= "Result Not Found for {}".format(usn))
+
+    else:
+        Found = render_template("found", result = result)
+        card_content = BeautifulSoup(Found)
+        return statement (Found).simple_card(title=result["sgpa"], content= card_content.text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
